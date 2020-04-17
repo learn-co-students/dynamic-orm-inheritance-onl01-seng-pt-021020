26982fdd3f87ce6d4e07c9bf3f8f4b6be5cd8b45
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-17 02:40:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,5 +5,7 @@ class Song < InteractiveRecord
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
+  
+  #The only code the Song class needs to contain is the code to create the attr_accessors specific to itself. But even that code uses a method, #column_names, inherited from the super class.
 
 end
